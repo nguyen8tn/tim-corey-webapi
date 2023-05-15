f43ff6fca7ccfe996e310153f10abf4ea0e9a3a4
NguyenNC14 <nguyennc14@fpt.com.vn>
2023-05-15 17:55:00 +0700

    add auth

--- TimCoreyWebApi/Program.cs
+++ TimCoreyWebApi/Program.cs
@@ -1,3 +1,8 @@
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+using ApiSecurity.Constants;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +11,41 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddAuthorization(opts =>
+{
+    opts.AddPolicy(PolicyConstants.MustHaveEmployeeId, policy =>
+    {
+        policy.RequireClaim("employeeId");
+    });
+
+    opts.AddPolicy(PolicyConstants.MustBeTheOwner, policy =>
+    {
+        policy.RequireClaim("title", "Business Owner");
+    });
+
+    opts.AddPolicy(PolicyConstants.MustBeAVeteranEmployee, policy =>
+    {
+        policy.RequireClaim("employeeId", "E001", "E002", "E003");
+    });
+
+    opts.FallbackPolicy = new AuthorizationPolicyBuilder()
+        .RequireAuthenticatedUser()
+        .Build();
+});
+builder.Services.AddAuthentication("Bearer")
+    .AddJwtBearer(opts =>
+    {
+        opts.TokenValidationParameters = new()
+        {
+            ValidateIssuer = true,
+            ValidateAudience = true,
+            ValidateIssuerSigningKey = true,
+            ValidIssuer = builder.Configuration.GetValue<string>("Authentication:Issuer"),
+            ValidAudience = builder.Configuration.GetValue<string>("Authentication:Audience"),
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(
+                builder.Configuration.GetValue<string>("Authentication:SecretKey")))
+        };
+    });
 
 var app = builder.Build();
 
@@ -18,6 +58,7 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
